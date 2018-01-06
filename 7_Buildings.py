5ac836978d0d4a726173575bffd561d3cf49ee52
Vignesh Hari <vichuhari100@gmail.com>
2018-01-06 23:37:50 +0530

    Reworded 3rd question

--- 7_Buildings.py
+++ 7_Buildings.py
@@ -1,10 +1,10 @@
 '''
 Question
-Motu is a skydiver. Every time he jumps off an airplane, he tries to land on one of the highest available buildings in his premises. Given the heighti of all available buildings in the locality, calculate the number of buildings on which he can land.
+Motu is a skydiver. Every time he jumps off an airplane, he tries to land on one of the highest available buildings in his premises. Given the height of all available buildings in the locality, calculate the number of buildings on which he can land.
 
 Input 
-First line contains an integer n denoting the total number of buildings in the area.
-The second line contains n space-separated integers, where each integer i denotes the height of the ith building.
+First line contains an integer N denoting the total number of buildings in the area.
+The second line contains N space-separated integers, where each integer I denotes the height of the Ith building.
 
 Output
 Print the available number of buildings on a new line.
@@ -21,8 +21,8 @@ Here the buildings with height 8 has the maximum heights. As there are 2 such bu
 
 Constraints
 
-1 <= n <= 100
-1 <= height <= 1000
+1 <= N <= 100
+1 <= Height <= 1000
 
 '''
 
